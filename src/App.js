720e1bdff34d5ca965ef09788517b283d9571f5c
0xkirthik <kirthiktester@gmail.com>
2024-11-27 12:17:41 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import { Routes, Route } from 'react-router-dom';
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home/>}/>
+      <Route path="/" index element={<Home/>}/>
       <Route path="/about" element={<About/>}/>
       <Route path="/tours" element={<Tour/>}/>
       <Route path="/contact" element={<Contact/>}/>
